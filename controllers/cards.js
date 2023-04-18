06881d279d6a9857b621ac35eb0c834b10845e1b
azatprojazz <azatprojazz@gmail.com>
2023-04-18 20:24:08 +0300

    fix: сделать контроллерам лайки и ассинхронный код

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,24 +1,70 @@
 const Card = require('../models/card');
 
-module.exports.getCards = (req, res) => {
-  Card.find({})
-    .populate(['owner', 'likes'])
-    .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+const getCards = async (req, res) => {
+  try {
+    const cards = await Card.find({}).populate(['owner', 'likes']);
+    res.send({ data: cards });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 
-module.exports.createCard = (req, res) => {
+const createCard = async (req, res) => {
   const { name, link } = req.body;
 
-  Card.create({ name, link, owner: req.user._id })
-    .then((card) => card.populate(['owner']))
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  try {
+    const card = await Card.create({ name, link, owner: req.user._id });
+    const populatedCard = await card.populate(['owner']);
+    res.send({ data: populatedCard });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 
-module.exports.deleteCardById = (req, res) => {
-  Card.findByIdAndRemove(req.params.cardId)
-    .populate(['owner', 'likes'])
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+const deleteCardById = async (req, res) => {
+  try {
+    const card = await Card.findByIdAndRemove(req.params.cardId).populate(['owner', 'likes']);
+    res.send({ data: card });
+  } catch (err) {
+    res.status(500).send({ message: 'Произошла ошибка' });
+  }
+};
+
+const likeCard = async (req, res) => {
+  try {
+    const updatedCard = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } },
+      { new: true }
+    ).populate(['owner', 'likes']);
+
+    res.status(200).json(updatedCard);
+  } catch (error) {
+    res.status(500).json({ message: 'Произошла ошибка' });
+  }
+};
+
+const dislikeCard = async (req, res) => {
+  try {
+    const updatedCard = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      {
+        $pull: { likes: req.user._id },
+      },
+      { new: true }
+    ).populate(['owner', 'likes']);
+
+    res.status(200).json(updatedCard);
+  } catch (error) {
+    res.status(500).json({ message: 'Произошла ошибка' });
+  }
+};
+
+
+module.exports = {
+  getCards,
+  createCard,
+  deleteCardById,
+  likeCard,
+  dislikeCard,
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,12 @@
 const User = require('../models/user');
 
-module.exports.getUsers = (req, res) => {
+const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
-module.exports.createUser = (req, res) => {
+const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
@@ -14,8 +14,48 @@ module.exports.createUser = (req, res) => {
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
-module.exports.getUserById = (req, res) => {
+const getUserById = (req, res) => {
   User.findById(req.params.id)
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    // Передадим объект опций:
+    {
+      new: true, // обработчик then получит на вход обновлённую запись
+      runValidators: true, // данные будут валидированы перед изменением
+      upsert: true, // если пользователь не найден, он будет создан
+    }
+  )
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    // Передадим объект опций:
+    {
+      new: true, // обработчик then получит на вход обновлённую запись
+      runValidators: true, // данные будут валидированы перед изменением
+      upsert: true, // если пользователь не найден, он будет создан
+    }
+  )
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports = {
+  getUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  updateAvatar
 };
