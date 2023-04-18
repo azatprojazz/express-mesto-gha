e41227d15c382f52536a60fb847a1549273716ac
azatprojazz <azatprojazz@gmail.com>
2023-04-18 16:13:32 +0300

    fix: подключить необходимое в app.js

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const url = 'mongodb://localhost:27017/mestodb';
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
 });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: '643e799303557cb9cbf8a927', // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен ${PORT}`);
-});
-
-const url = 'mongodb://localhost:27017/mestodb';
-
-MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-  if (err) {
-    console.error('Ошибка при подключении к MongoDB:', err);
-    return;
-  }
-  console.log('Подключено к MongoDB');
 });
