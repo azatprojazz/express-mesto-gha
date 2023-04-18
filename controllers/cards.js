e11565109979d91e399bbb7ee744910d39f0a8c2
azatprojazz <azatprojazz@gmail.com>
2023-04-18 16:13:50 +0300

    feat: сделать контроллеры

---
+++ controllers/cards.js
@@ -0,0 +1,24 @@
+const Card = require('../models/card');
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .populate(['owner', 'likes'])
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => card.populate(['owner']))
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.deleteCardById = (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId)
+    .populate(['owner', 'likes'])
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+};
---
+++ controllers/users.js
@@ -0,0 +1,21 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+};
