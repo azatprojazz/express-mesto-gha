5c967a5ca7dc03e62cd607071c78f5a06653f393
azatprojazz <azatprojazz@gmail.com>
2023-04-18 16:14:09 +0300

    feat: сделать модели

---
+++ models/card.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema.Types;
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: {
+    type: [ObjectId],
+    ref: 'user',
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
