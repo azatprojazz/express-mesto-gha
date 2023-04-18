831d400ff7dabab33816993524b3ca745d9d9806
azatprojazz <azatprojazz@gmail.com>
2023-04-18 20:22:37 +0300

    fix: добавить роуты лайков

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-const { getCards, createCard, deleteCardById } = require('../controllers/cards');
+const { getCards, createCard, deleteCardById, likeCard, dislikeCard } = require('../controllers/cards');
 
 router.get('/', getCards);
 router.post('/', createCard);
 router.delete('/:cardId', deleteCardById);
+
+router.put('/:cardId/likes', likeCard)
+router.delete('/:cardId/likes', dislikeCard)
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
-const { getUsers, createUser, getUserById } = require('../controllers/users');
+const { getUsers, createUser, getUserById, updateUser, updateAvatar } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.post('/', createUser);
 
 router.get('/:id', getUserById);
+
+router.patch('/me', updateUser);
+router.patch('/me/avatar', updateAvatar)
 
 module.exports = router;
