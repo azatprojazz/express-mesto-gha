d0c7b71d39a5746f2c94ef082b9e14a46da1a241
azatprojazz <azatprojazz@gmail.com>
2023-04-19 14:30:53 +0300

    feat: добавить обработку всех ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,11 +1,19 @@
+const { DocumentNotFoundError, CastError, ValidationError } = require('mongoose').Error;
 const Card = require('../models/card');
+
+const {
+  HTTP_STATUS_CREATED,
+  HTTP_STATUS_BAD_REQUEST,
+  HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_INTERNAL_SERVER_ERROR,
+} = require('../utils/constants');
 
 const getCards = async (req, res) => {
   try {
     const cards = await Card.find({}).populate(['owner', 'likes']);
     res.send({ data: cards });
   } catch (err) {
-    res.status(500).send({ message: err.message });
+    res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: err.message });
   }
 };
 
@@ -15,18 +23,35 @@ const createCard = async (req, res) => {
   try {
     const card = await Card.create({ name, link, owner: req.user._id });
     const populatedCard = await card.populate(['owner']);
-    res.send({ data: populatedCard });
+    res.status(HTTP_STATUS_CREATED).send({ data: populatedCard });
   } catch (err) {
-    res.status(500).send({ message: err.message });
+    console.log(err);
+    if (err instanceof ValidationError) {
+      const errorMessage = Object.values(err.errors)
+        .map((error) => error.message)
+        .join(' ');
+      res.status(HTTP_STATUS_BAD_REQUEST).send({ message: `Переданы некорректные данные ${errorMessage}` });
+    } else {
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: err.message });
+    }
   }
 };
 
 const deleteCardById = async (req, res) => {
   try {
     const card = await Card.findByIdAndRemove(req.params.cardId).populate(['owner', 'likes']);
-    res.send({ data: card });
+
+    if (!card) {
+      res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Карточка с таким ID не найдена' });
+    } else {
+      res.send({ data: card });
+    }
   } catch (err) {
-    res.status(500).send({ message: 'Произошла ошибка' });
+    if (err instanceof CastError) {
+      res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Неверный формат идентификатора карточки' });
+    } else {
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' });
+    }
   }
 };
 
@@ -37,10 +62,24 @@ const likeCard = async (req, res) => {
       { $addToSet: { likes: req.user._id } },
       { new: true }
     ).populate(['owner', 'likes']);
-
-    res.status(200).json(updatedCard);
+    if (!updatedCard) {
+      res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Карточка с таким ID не найдена' });
+    } else {
+      res.status(200).json(updatedCard);
+    }
   } catch (error) {
-    res.status(500).json({ message: 'Произошла ошибка' });
+    if (error instanceof ValidationError) {
+      const errorMessage = Object.values(err.errors)
+        .map((error) => error.message)
+        .join(' ');
+      res
+        .status(HTTP_STATUS_BAD_REQUEST)
+        .send({ message: `Переданы некорректные данные для постановки/снятии лайка ${errorMessage}` });
+    } else if (error instanceof CastError) {
+      res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Неверный формат идентификатора карточки или пользователя' });
+    } else {
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ message: 'Произошла ошибка' });
+    }
   }
 };
 
@@ -53,13 +92,26 @@ const dislikeCard = async (req, res) => {
       },
       { new: true }
     ).populate(['owner', 'likes']);
-
-    res.status(200).json(updatedCard);
+    if (!updatedCard) {
+      res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Карточка с таким ID не найдена' });
+    } else {
+      res.status(200).json(updatedCard);
+    }
   } catch (error) {
-    res.status(500).json({ message: 'Произошла ошибка' });
+    if (error instanceof ValidationError) {
+      const errorMessage = Object.values(err.errors)
+        .map((error) => error.message)
+        .join(' ');
+      res
+        .status(HTTP_STATUS_BAD_REQUEST)
+        .send({ message: `Переданы некорректные данные для постановки/снятии лайка ${errorMessage}` });
+    } else if (error instanceof CastError) {
+      res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Неверный формат идентификатора карточки или пользователя' });
+    } else {
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ message: 'Произошла ошибка' });
+    }
   }
 };
-
 
 module.exports = {
   getCards,
--- controllers/users.js
+++ controllers/users.js
@@ -1,55 +1,117 @@
+const { DocumentNotFoundError, CastError, ValidationError } = require('mongoose').Error;
 const User = require('../models/user');
 
-const getUsers = (req, res) => {
-  User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+const {
+  HTTP_STATUS_CREATED,
+  HTTP_STATUS_BAD_REQUEST,
+  HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_INTERNAL_SERVER_ERROR,
+} = require('../utils/constants');
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send({ data: users });
+  } catch (err) {
+    res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: err.message });
+  }
 };
 
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
   const { name, about, avatar } = req.body;
 
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  try {
+    const user = await User.create({ name, about, avatar });
+    res.send({ data: user });
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      const errorMessage = Object.values(err.errors)
+        .map((error) => error.message)
+        .join(' ');
+      res.status(HTTP_STATUS_BAD_REQUEST).send({ message: `Переданы некорректные данные ${errorMessage}` });
+    } else {
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: err.message });
+    }
+  }
 };
 
-const getUserById = (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).orFail();
+    res.send({ data: user });
+  } catch (err) {
+    if (err instanceof DocumentNotFoundError) {
+      res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Пользователя нет в базе' });
+    } else if (err instanceof CastError) {
+      res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Неверный формат идентификатора пользователя' });
+    } else {
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' });
+    }
+  }
 };
 
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(
-    req.user._id,
-    { name, about },
-    // Передадим объект опций:
-    {
-      new: true, // обработчик then получит на вход обновлённую запись
-      runValidators: true, // данные будут валидированы перед изменением
-      upsert: true, // если пользователь не найден, он будет создан
+
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { name, about },
+      {
+        new: true, // обработчик then получит на вход обновлённую запись
+        runValidators: true, // данные будут валидированы перед изменением
+        upsert: true, // если пользователь не найден, он будет создан
+      }
+    ).orFail();
+    res.send({ data: user });
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      const errorMessage = Object.values(err.errors)
+        .map((error) => error.message)
+        .join(' ');
+      res
+        .status(HTTP_STATUS_BAD_REQUEST)
+        .send({ message: `Переданы некорректные данные при обновлении профиля ${errorMessage}` });
+    } else if (err instanceof DocumentNotFoundError) {
+      res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Пользователь с указанным ID не найден' });
+    } else if (err instanceof CastError) {
+      res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Неверный формат идентификатора пользователя' });
+    } else {
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' });
     }
-  )
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+  }
 };
 
-const updateAvatar = (req, res) => {
+const updateAvatar = async (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(
-    req.user._id,
-    { avatar },
-    // Передадим объект опций:
-    {
-      new: true, // обработчик then получит на вход обновлённую запись
-      runValidators: true, // данные будут валидированы перед изменением
-      upsert: true, // если пользователь не найден, он будет создан
+
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { avatar },
+      {
+        new: true,
+        runValidators: true,
+        upsert: true,
+      }
+    ).orFail();
+    res.send({ data: user });
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      const errorMessage = Object.values(err.errors)
+        .map((error) => error.message)
+        .join(' ');
+      res
+        .status(HTTP_STATUS_BAD_REQUEST)
+        .send({ message: `Переданы некорректные данные при обновлении аватара ${errorMessage}` });
+    } else if (err instanceof DocumentNotFoundError) {
+      res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Пользователь с указанным ID не найден' });
+    } else if (err instanceof CastError) {
+      res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Неверный формат идентификатора пользователя' });
+    } else {
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' });
     }
-  )
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+  }
 };
 
 module.exports = {
@@ -57,5 +119,5 @@ module.exports = {
   createUser,
   getUserById,
   updateUser,
-  updateAvatar
+  updateAvatar,
 };
