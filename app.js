479b6513027387fd17f2237d378a6d7115a609a9
azatprojazz <azatprojazz@gmail.com>
2023-04-20 16:03:26 +0300

    fix: улучшить app.js

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const url = 'mongodb://localhost:27017/mestodb';
-
-mongoose.connect(url, {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -17,17 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
-    _id: '643e799303557cb9cbf8a927', // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '64404d6a4e10670269c46adf', // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
-
   next();
 });
 
 app.use(router);
 
-// Обработка ошибки 404
-app.use((req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+app.listen(PORT, () => {
+  console.log(`Сервер слушает порт => ${PORT}`);
 });
-
-app.listen(PORT, () => {});
