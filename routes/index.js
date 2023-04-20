cb0c985ddaf9c4b83311a89cfa56d87fdd6c7c20
azatprojazz <azatprojazz@gmail.com>
2023-04-20 16:12:07 +0300

    fix: перенести из app.js в index.js роут ошибки

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
-
 const users = require('./users');
 const cards = require('./cards');
+const { NOT_FOUND_404 } = require('../utils/constants');
 
 router.use('/users', users);
 router.use('/cards', cards);
+
+router.use((_, res) => {
+  res.status(NOT_FOUND_404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 module.exports = router;
