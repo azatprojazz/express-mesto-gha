5bb91807cb1364b402351d73dd3127ebbce9e844
azatprojazz <azatprojazz@gmail.com>
2023-05-02 21:33:12 +0300

    fix: подключить errorMiddleware в app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const router = require('./routes/index');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const { PORT = 3000 } = process.env;
 
@@ -13,14 +15,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use((req, res, next) => {
-  req.user = {
-    _id: '64404d6a4e10670269c46adf', // вставьте сюда _id созданного в предыдущем пункте пользователя
-  };
-  next();
-});
+app.use(cookieParser());
 
 app.use(router);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Сервер слушает порт => ${PORT}`);
