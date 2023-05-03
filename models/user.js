58dae01a8e3c8ef3afe044b05ab84b34891864a0
azatprojazz <azatprojazz@gmail.com>
2023-05-03 20:55:29 +0300

    fix: оптимизировать errorMiddleware.js

--- models/user.js
+++ models/user.js
@@ -45,20 +45,20 @@ const userSchema = new mongoose.Schema(
 
 // eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password')
+  return this.findOne({ email })
+    .select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
       }
 
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-          }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+        }
 
-          return user; // теперь user доступен
-        });
+        return user; // теперь user доступен
+      });
     });
 };
 
