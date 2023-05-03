af6bf45a8975c434a4fed06b95fb252dba2144c4
azatprojazz <azatprojazz@gmail.com>
2023-05-03 20:53:54 +0300

    fix: оптимизировать auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,12 @@
 const jwt = require('jsonwebtoken');
 
-const {
-  UNAUTHORIZED_401,
-} = require('../utils/constants');
+const { UNAUTHORIZED_401 } = require('../utils/constants');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
 
   if (!token) {
-    return res
-      .status(UNAUTHORIZED_401)
-      .send({ message: 'Необходима авторизация' });
+    return res.status(UNAUTHORIZED_401).send({ message: 'Необходима авторизация' });
   }
 
   let payload;
@@ -18,9 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return res
-      .status(UNAUTHORIZED_401)
-      .send({ message: 'Необходима авторизация' });
+    return res.status(UNAUTHORIZED_401).send({ message: 'Необходима авторизация' });
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
