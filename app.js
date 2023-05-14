3f459d70867c7ebf595fa13f7480f241f23d8d1b
azatprojazz <azatprojazz@gmail.com>
2023-05-14 18:36:20 +0300

    feat: реализовать логи

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const errorMiddleware = require('./middlewares/errorMiddleware');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -18,9 +19,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.use(router);
+
+app.use(errorLogger); // подключаем логгер ошибок
+
 app.use(errors());
 app.use(errorMiddleware);
+
 
 app.listen(PORT, () => {
   console.log(`Сервер слушает порт => ${PORT}`);
